7ef44b8e53a2ab7ac072db0f4558faedda21b442
BlissYang <2812420513@qq.com>
2019-09-20 18:08:00 +0800

    引入scss

--- src/components/Horiz_Show_List.vue
+++ src/components/Horiz_Show_List.vue
@@ -43,7 +43,7 @@ export default {
 }
 </script>
 
-<style  scoped >
+<style lang="scss"  scoped >
 .scroller{
    width: 100%;
    height: 100%;
@@ -75,6 +75,9 @@ export default {
    align-items: center;
    justify-content: center;
    list-style: none;
+   >img{
+      
+   }
   
 }
 </style>
